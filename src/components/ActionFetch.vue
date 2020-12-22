19c5c8d9514c328398c6c709dea80ea250a7ea77
Levi <54490191+SaltedyLevity@users.noreply.github.com>
2020-12-21 22:27:13 -0500

    restructuring the html template for better tooltip cap implementation, and updated associated and needed styles. WILL NOT WORK OUTRIGHT pls help me with vue stuff.

--- src/components/ActionFetch.vue
+++ src/components/ActionFetch.vue
@@ -89,16 +89,41 @@ export default {
             class="xivtooltip-inline"
         >
             <!-- eslint-enable -->
-            <span :class="[name ? 'xivtooltip-inline-image' : 'xivtooltip-thumbnail']"><img
-                :class="[name ? 'xivtooltip-inline-image' : 'xivtooltip-thumbnail']"
-                :src="icon"
-            ></span><span
+            <!--levi's bad attempts at this start here -->
+            <div :class="[name ? 'xivtooltip-inline-image-wrapper' : 'xivtooltip-icon-image-wrapper']">
+                <img
+                    :class="[name ? 'xivtooltip-inline-image' : 'xivtooltip-icon-image']"
+                    :src="icon"
+                ><!-- cap image going in the template for better positioning, no idea how to format this--><img
+                    :class="[name ? 'xivtooltip-inline-cap' : 'xivtooltip-icon-cap']"
+                    :src="'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAMAAABg3Am1AAAA7VBMVEUAAAAAAAD////////8/Pz///////8AAAD////////////////+/v7///8AAAAAAAD////y8vIAAAAAAAD9/f0AAAD9/f3+/v7+/v4AAAD///////8AAAD///8cHBwJCQlvb28AAAAAAAD////x8fH+/v6FhYULCwsFBQUjIyMMDAx2dnZhYWEXFxesrKxBQUECAgLX19cMDAz09PS6urpGRkZZWVkzMzMKCgpdXV3///8AAAAGBgZeXl4AAAAFBQX///9XV1cAAAA3Nze1tbX///+zs7OgoKAbGxs4ODgAAADz8/MICAgsLCyNjY0qfaSCAAAATHRSTlMAIwMLG0k9DC9QNUQnq5tfVTMJAx0eERYOFmZZQQfiim9hUDk1IPf18eza19DMy8rKtbGurKupopuYk5ORgnl2c2RXVkpFPTY1MjInha+GRQAAAVdJREFUSMft1tdOwzAUgOFmNRQKFDtxdtus7r333g28/+MQJWkrLnAiIXGV/8aWjj/JlyfhBSEALDYAIEzcguA8rha+sBWq4zOAt/fbWqPdfcXWbTdq20DAU7M3WclKBpMirya95skHIDeQ5UwqpIwsD3LAA2wrr6SY0FJKvsV6QCodFGb/EtKeUQ4lyQO8YzPJCDG2wweAS5rPoZlJLgCEQ5tPETJph7gBAQm0ncZl024PQJHuSWqUKCJBcCecn3sTBCSKlEa6Y+onCCkGMYhBDP4CuGiAewAxChDvoHhEWjjQ0LEYgMpaoMjwHwnrig/4zvCKKI3EplHoOuzwPlhm52kaiRQmEdHpeXbpA8kYZfv5nWV9/Jpl7fL97MiQPMCq+qxe/gypXJ/pKusBQKj6Ijd9wzbNLXSVAB6ALHFRjc07to2hXgjpvjuwEs8T2HheYgFM/EffzfVXvQZrTbEAAAAASUVORK5CYII='"
+                ><span
                 v-if="name"
                 class="xivtooltip-inline-underline"
             >{{ name }}</span>
         </div>
     </Popper>
 </template>
+
+/* expected output for these is the following:
+If inline:
+<div class="xivtoolt1ip-inline">
+        <div class="xivtooltip-inline-image-wrapper">
+              <img src="https://xivapi.com/i/002000/002632.png" class="xivtooltip-inline-image"/>
+              <img src="data:image/png;base64foo" class="xivtooltip-inline-cap">
+        </div>
+<span class="xivtooltip-inline-underline">Asylum</span>
+</div>
+
+if icon:
+<div class="xivtooltip-inline">
+        <div class="xivtooltip-icon-image-wrapper">
+              <img src="https://xivapi.com/i/002000/002848.png" class="xivtooltip-icon-image"/>
+          <img src="data:image/png;base64foo" class="xivtooltip-icon-cap" >
+        </div>
+</div>
+*/
+
 
 <style scoped>
 .popper {
@@ -129,40 +154,56 @@ export default {
 	border-bottom: dotted #AFAFAF 2px;
 }
 
-.xivtooltip-inline-image {
-  width: 1.45em;
-  height: 1.45em;
+/* TOOLTIP CAP COVER IMAGE */
+
+/*inline*/
+.xivtooltip-inline-image-wrapper {
+position: relative;
+	display: inline;
+	margin-right: 1.4em;
+	padding-right: 8px;
+}
+.xivtooltip-inline-cap {
+  width: 1.68em;
+  height: 1.68em;
   vertical-align: middle;
   margin-bottom: 1.7px;
   margin-right: .18em;
   margin-top: -1.7px;
   margin-left: 2px;
+  position: absolute;	
+  left: -.15em;
+  top: -.08em;
+	
 }
+.xivtooltip-inline-image {
+  width: 1.4em;
+  height: 1.4em;
+  vertical-align: middle;
+  margin-bottom: 1.7px;
+  margin-right: .18em;
+  margin-top: -1.7px;
+  margin-left: 2px;
+  position: absolute;
 
-/* TOOLTIP CAP COVER IMAGE */
-
-.xivtooltip-thumbnail::before{
-  content: '';
-  background: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAMAAABg3Am1AAAA7VBMVEUAAAAAAAD////////8/Pz///////8AAAD////////////////+/v7///8AAAAAAAD////y8vIAAAAAAAD9/f0AAAD9/f3+/v7+/v4AAAD///////8AAAD///8cHBwJCQlvb28AAAAAAAD////x8fH+/v6FhYULCwsFBQUjIyMMDAx2dnZhYWEXFxesrKxBQUECAgLX19cMDAz09PS6urpGRkZZWVkzMzMKCgpdXV3///8AAAAGBgZeXl4AAAAFBQX///9XV1cAAAA3Nze1tbX///+zs7OgoKAbGxs4ODgAAADz8/MICAgsLCyNjY0qfaSCAAAATHRSTlMAIwMLG0k9DC9QNUQnq5tfVTMJAx0eERYOFmZZQQfiim9hUDk1IPf18eza19DMy8rKtbGurKupopuYk5ORgnl2c2RXVkpFPTY1MjInha+GRQAAAVdJREFUSMft1tdOwzAUgOFmNRQKFDtxdtus7r333g28/+MQJWkrLnAiIXGV/8aWjj/JlyfhBSEALDYAIEzcguA8rha+sBWq4zOAt/fbWqPdfcXWbTdq20DAU7M3WclKBpMirya95skHIDeQ5UwqpIwsD3LAA2wrr6SY0FJKvsV6QCodFGb/EtKeUQ4lyQO8YzPJCDG2wweAS5rPoZlJLgCEQ5tPETJph7gBAQm0ncZl024PQJHuSWqUKCJBcCecn3sTBCSKlEa6Y+onCCkGMYhBDP4CuGiAewAxChDvoHhEWjjQ0LEYgMpaoMjwHwnrig/4zvCKKI3EplHoOuzwPlhm52kaiRQmEdHpeXbpA8kYZfv5nWV9/Jpl7fL97MiQPMCq+qxe/gypXJ/pKusBQKj6Ijd9wzbNLXSVAB6ALHFRjc07to2hXgjpvjuwEs8T2HheYgFM/EffzfVXvQZrTbEAAAAASUVORK5CYII=');
+}
+/*icon*/
+.xivtooltip-icon-image-wrapper {
+	position: relative;
+	display: inline;
+	margin-right: 40px;
+}
+.xivtooltip-icon-image {
+	width: 40px;
+	height: 40px;
+	position: absolute;
+	bottom: 0px;
+	left: 0px
+}
+.xivtooltip-icon-cap {
+	width: 48px;
   height: 48px;
-  width: 48px;
-  background-size: contain;
-  position: absolute;
-  background-repeat: no-repeat;
-  left: -4px;
-  bottom: -3px;
+  position: absolute;	
+	left: -4px;
+	bottom: -6px;
 }
-
-.xivtooltip-inline-image::before{
-  content: '';
-  background: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAMAAABg3Am1AAAA7VBMVEUAAAAAAAD////////8/Pz///////8AAAD////////////////+/v7///8AAAAAAAD////y8vIAAAAAAAD9/f0AAAD9/f3+/v7+/v4AAAD///////8AAAD///8cHBwJCQlvb28AAAAAAAD////x8fH+/v6FhYULCwsFBQUjIyMMDAx2dnZhYWEXFxesrKxBQUECAgLX19cMDAz09PS6urpGRkZZWVkzMzMKCgpdXV3///8AAAAGBgZeXl4AAAAFBQX///9XV1cAAAA3Nze1tbX///+zs7OgoKAbGxs4ODgAAADz8/MICAgsLCyNjY0qfaSCAAAATHRSTlMAIwMLG0k9DC9QNUQnq5tfVTMJAx0eERYOFmZZQQfiim9hUDk1IPf18eza19DMy8rKtbGurKupopuYk5ORgnl2c2RXVkpFPTY1MjInha+GRQAAAVdJREFUSMft1tdOwzAUgOFmNRQKFDtxdtus7r333g28/+MQJWkrLnAiIXGV/8aWjj/JlyfhBSEALDYAIEzcguA8rha+sBWq4zOAt/fbWqPdfcXWbTdq20DAU7M3WclKBpMirya95skHIDeQ5UwqpIwsD3LAA2wrr6SY0FJKvsV6QCodFGb/EtKeUQ4lyQO8YzPJCDG2wweAS5rPoZlJLgCEQ5tPETJph7gBAQm0ncZl024PQJHuSWqUKCJBcCecn3sTBCSKlEa6Y+onCCkGMYhBDP4CuGiAewAxChDvoHhEWjjQ0LEYgMpaoMjwHwnrig/4zvCKKI3EplHoOuzwPlhm52kaiRQmEdHpeXbpA8kYZfv5nWV9/Jpl7fL97MiQPMCq+qxe/gypXJ/pKusBQKj6Ijd9wzbNLXSVAB6ALHFRjc07to2hXgjpvjuwEs8T2HheYgFM/EffzfVXvQZrTbEAAAAASUVORK5CYII=');
-  height: 1.7em;
-  width: 1.7em;
-  background-size: contain;
-  position: absolute;
-  background-repeat: no-repeat;
-  left: 2px;
-  top: -4px;
-}
-
-</style>
