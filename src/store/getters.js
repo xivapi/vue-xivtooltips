c026f0b3f860fa7037f21643e568086d6bef4b19
Ay'yaruq Dotharl <acchan.ff14@gmail.com>
2021-01-30 16:01:31 +1100

    Properly null check getters

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,9 +1,11 @@
 export default {
     getActionById: (state) => (id, lang) => {
-        return state.actions[id][lang]
+        return state.actions[id] ? state.actions[id][lang] : undefined
     },
     getIconById: (state) => (id, lang) => {
-        return state.actions[id][lang] && state.actions[id][lang].Icon ? `https://xivapi.com${state.actions[id][lang].Icon}` : ''
+        return (state.actions[id] && state.actions[id][lang] && state.actions[id][lang].Icon)
+          ? `https://xivapi.com${state.actions[id][lang].Icon}`
+          : ''
     },
     getTimeout: (state) => () => state.defaultTimeout,
 }
